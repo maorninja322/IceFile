0dea6724c3ccd3970ef9d1687585bf82ca268fba
StarLand <smirkogm@gmail.com>
2015-03-13 20:12:28 +0100

    Create remove.php

---
+++ sites/remove.php
@@ -0,0 +1,13 @@
+<div class="greyer"><h2>Remove</h2>
+<?php if($loguserid)
+{
+	echo '
+	<div align="center">
+Feature not finished yet
+	</div>';
+}
+if(!$loguserid) {
+echo '<div align="center">Sign in or Sign up to remove files</div>';	
+}
+?>
+</div>
